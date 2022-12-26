c1c8c08929dc7bb712b22c3fef8379f850f775b5
carraes <carraeshb@gmail.com>
2022-12-26 10:56:43 -0300

    refactor: index, add static fromCsv to MatchReader

--- build/index.js
+++ build/index.js
@@ -1,13 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const WinAnalysis_1 = require("./analyzers/WinAnalysis");
-const CsvFileReader_1 = require("./CsvFileReader");
 const MatchReader_1 = require("./MatchReader");
-// import { ConsoleReport } from './reportTargets/ConsoleReport'
-const HtmlReport_1 = require("./reportTargets/HtmlReport");
 const Summary_1 = require("./Summary");
-const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
-const summary = new Summary_1.Summary(new WinAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
+const summary = Summary_1.Summary.winsAnalysisWithConsoleReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import { WinsAnalysis } from './analyzers/WinAnalysis'
-import { CsvFileReader } from './CsvFileReader'
 import { MatchReader } from './MatchReader'
-// import { ConsoleReport } from './reportTargets/ConsoleReport'
-import { HtmlReport } from './reportTargets/HtmlReport'
 import { Summary } from './Summary'
 
-const csvFileReader = new CsvFileReader('football.csv')
-const matchReader = new MatchReader(csvFileReader)
+const matchReader = MatchReader.fromCsv('football.csv')
 matchReader.load()
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport())
+const summary = Summary.winsAnalysisWithConsoleReport('Man United')
 
 summary.buildAndPrintReport(matchReader.matches)
