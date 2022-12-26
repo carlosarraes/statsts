a2c3bebabca779cef60bb8df06727bf94f4b11c0
carraes <carraeshb@gmail.com>
2022-12-26 10:57:24 -0300

    refactor: index

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,7 @@ import { MatchReader } from './MatchReader'
 import { Summary } from './Summary'
 
 const matchReader = MatchReader.fromCsv('football.csv')
-matchReader.load()
-
 const summary = Summary.winsAnalysisWithConsoleReport('Man United')
 
+matchReader.load()
 summary.buildAndPrintReport(matchReader.matches)
