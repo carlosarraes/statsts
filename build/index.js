ac824c480a9bb666912c27014c0eda0279734d2a
carraes <carraeshb@gmail.com>
2022-12-26 08:36:20 -0300

    feat: CsvFileReader

--- build/index.js
+++ build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hello');
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+let manUnitedWins = 0;
+for (const match of reader.data) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins += 1;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins += 1;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
